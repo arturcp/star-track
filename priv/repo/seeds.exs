e91a2fb8d30975bcdf7bbf077caa4bd861239e53
Artur <artur.prado@gmail.com>
2020-04-26 18:46:35 -0300

    Add new characters

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -23,13 +23,31 @@ Repo.insert!(%Character{
 })
 
 Repo.insert!(%Character{
+  name: "Sam",
+  biography: "Nullam orci elit, iaculis at gravida non, imperdiet quis mauris. Nam sed lacinia purus, sit amet finibus nisi. Donec varius consectetur metus, in ullamcorper ipsum venenatis ut.",
+  image_url: "https://iili.io/JA7tKN.jpg"
+})
+
+Repo.insert!(%Character{
   name: "Diego",
   biography: "Pellentesque eget diam ullamcorper, vestibulum leo sed, ornare odio. Vivamus non eros purus. Quisque volutpat neque ipsum, ut varius justo gravida vel. Nam et arcu a tellus pulvinar scelerisque.",
   image_url: "https://iili.io/JAIMas.jpg"
 })
 
 Repo.insert!(%Character{
-  name: "Sam",
-  biography: "Nullam orci elit, iaculis at gravida non, imperdiet quis mauris. Nam sed lacinia purus, sit amet finibus nisi. Donec varius consectetur metus, in ullamcorper ipsum venenatis ut.",
-  image_url: "https://iili.io/JA7tKN.jpg"
+  name: "Carolina",
+  biography: "Cras volutpat venenatis tristique. Sed aliquet dui vel orci faucibus porta. Phasellus eget quam at arcu convallis consequat id eget dolor. Pellentesque lacinia nisi sed augue blandit, sed feugiat neque malesuada. Integer commodo pellentesque libero, non luctus ligula bibendum a.",
+  image_url: "https://iili.io/JAYwJt.jpg"
+})
+
+Repo.insert!(%Character{
+  name: "Tarcísio",
+  biography: "Nulla nec fermentum mauris, sed tempor ligula. Vivamus mattis mollis libero, sit amet dignissim sapien lobortis vel. Nam in sagittis justo, sit amet volutpat sapien. Nullam faucibus ex eu mi congue porttitor.",
+  image_url: "https://iili.io/JAYOen.jpg"
+})
+
+Repo.insert!(%Character{
+  name: "Duda",
+  biography: "Nulla posuere eros orci, at vehicula ligula convallis eget. Curabitur feugiat augue in augue mattis, eu luctus justo tincidunt. Morbi ut tellus lectus. Duis sit amet porttitor metus, vitae varius erat. Proin tristique enim metus, id hendrerit tellus vehicula at.",
+  image_url: "https://iili.io/JAYN5X.jpg"
 })
