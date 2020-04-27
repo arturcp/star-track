760de030f56586f9dd6f244859ace39a76ed8e04
Artur <artur.prado@gmail.com>
2020-04-26 21:04:10 -0300

    Prepare deploy on heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,17 +10,17 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :star_track, StarTrackWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  load_from_system_env: true,
+  url: [scheme: "https", host: "pacific-shelf-29537.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  server: true,
-  secret_key_base: "${SECRET_KEY_BASE}"
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :star_track, StarTrack.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: "${DATABASE_URL}",
-  database: "",
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true,
-  pool_size: 1
+  url: System.get_env("DATABASE_URL")
 
 # Do not print debug messages in production
 config :logger, level: :info
